160a2e9ddd14a5b471f958932710d68eed894da4
salehjasim <91024624+salehjasim@users.noreply.github.com>
2022-12-25 22:52:42 +0000

    Deploying to gh-pages from @ salehjasim/project-19@abc1dbc6e778ac4919064a1449d55fc512cd06e1 🚀

---
+++ index.3c7f7763.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[read-more-modal-open]"),closeModalBtn:document.querySelector("[read-more-modal-close]"),modal:document.querySelector("[read-more-modal]")};function o(){e.modal.classList.toggle("is-hidden__modal")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.3c7f7763.js.map
