5ea56e27eea4f13f62957263cd1ff7fa178e0c8c
salehjasim <91024624+salehjasim@users.noreply.github.com>
2022-12-26 13:30:50 +0000

    Deploying to gh-pages from @ salehjasim/project-19@67a37f09e4bb0f58cb974f52568b2267a5629694 🚀

---
+++ index.bd2f2ac8.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[buyNow-modal-open]"),closeModalBtn:document.querySelector("[buyNow-modal-close]"),modal:document.querySelector("[buyNow-modal]")};function e(){o.modal.classList.toggle("buyNow-is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.bd2f2ac8.js.map
